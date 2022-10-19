b7c6439302092574b64824fbb38637a1f7a9e460
blueglass <cardanoviette123@gmail.com>
2022-10-19 16:39:22 -0500

    controllers updated

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -42,7 +42,7 @@ export const updatePost = async(req, res) => {
 
   if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
-  const updatedPost = await postMessage.findByIdAndUpdate(id, post, { new : true });
+  const updatedPost = await postModel.findByIdAndUpdate(id, post, { new : true });
 
   res.json(updatedPost);
 }
@@ -52,7 +52,7 @@ export const deletePost = async(req, res) => {
 
   if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
-  await postMessage.findByIdAndRemove(id);
+  await postModel.findByIdAndRemove(id);
 
   res.json( { message: 'Post deleted successfully' } );
 }
